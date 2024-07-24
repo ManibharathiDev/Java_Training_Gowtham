31a90163c00e160decf4046f75ba288089762bfe
Manibharathi <manibharath159@gmail.com>
2024-07-24 21:04:12 +0530

    Initial Setup with Variables and Data Types

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        // Variables & Data Types - Ranges
+        byte a = 10;
+
+        System.out.println("Byte Min Value "+Byte.MIN_VALUE);
+        System.out.println("Byte Max Value "+Byte.MAX_VALUE);
+
+        short b = 10;
+
+        System.out.println("Short Min Value "+Short.MIN_VALUE);
+        System.out.println("Short Max Value "+Short.MAX_VALUE);
+
+        int c = 10;
+        System.out.println("INT Min Value "+Integer.MIN_VALUE);
+        System.out.println("INT Max Value "+Integer.MAX_VALUE);
+
+        long d = 10;
+        System.out.println("Long Min Value "+Long.MIN_VALUE);
+        System.out.println("Long Max Value "+Long.MAX_VALUE);
+
+        float e = 10.9f;
+        System.out.println("Float Min Value "+Float.MIN_VALUE);
+        System.out.println("Float Max Value "+Float.MAX_VALUE);
+
+        double g = 10.9;
+        System.out.println("Double Min Value "+Double.MIN_VALUE);
+        System.out.println("Double Max Value "+Double.MAX_VALUE);
+
+
+    }
+}
