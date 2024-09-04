c674fda9ac47b0696d2c727c4e661513836c339a
Manibharathi <manibharath159@gmail.com>
2024-09-04 21:12:12 +0530

    String Methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,71 +1,120 @@
 package org.example;
 
+import java.sql.SQLSyntaxErrorException;
+
 public class Main {
 
     static int num = 1;
     public static void main(String[] args) {
-        String name = "gowtham ";
+       String s1 = "Gowtham";
+       String s2 = "Gowtham";
 
-//        System.out.println("My Name is "+name);
-//        System.out.println(name.length()); // Built In Mehtod
+       String name = new String("Gowtham");
 
-        char[] names = name.toCharArray();
-        int j = 0;
-        for(int i = 0; i<names.length;i++)
+       char[] ch = {'G','0','w','t','h','a','m'};
+       String charsString = new String(ch);
+       System.out.println(charsString);
+
+        /**
+         * Built In Functions
+         */
+        /**
+         * charAt(int index)
+         */
+
+        System.out.println(s1.charAt(2)); // It returns the 2nd index character
+        /**
+         * Length
+         */
+        System.out.println(s1.length());
+
+        /**
+         * substring(int beginIndex,int endIndex);
+         */
+
+        String s3 = "Hello World";
+        System.out.println(s3.substring(2));
+        System.out.println(s3.substring(2,7)); // (BeginIndex -> endIndex-1)
+
+        /**
+         * contains
+         * Check if the string contains such string
+         * It returns true, if it is present, else returns false
+         */
+
+        String s4 = "Hello World";
+        boolean res = s4.contains("Worlds");
+        System.out.println(res);
+        if(s4.contains("Hello")){
+            System.out.println("Strings are available");
+        }
+
+        /**
+         * isEmpty()
+         */
+
+        String s5 = "sdfsdfdsf";
+        if(s5.isEmpty())
         {
-            j++;
-            //System.out.println(names[i]);
+            System.out.println("Invalid String");
         }
 
-        //System.out.println(name.toCharArray()[0]);
-        // Find the length of string using own logic
-        //2. Print the reverse string
-        //charAt(index) => It returns the index character
-        String a = "MALAYALAMS";
+        /**
+         * concat(String newString)
+         * Join Strings
+         */
+        String s6 = "Udaya";
+        String s7 = "Kumar";
+        String s8 = s6.concat(s7);
+        s8 = s6.concat(" ").concat(s7);
+        System.out.println(s8);
 
-//        a = 10;
-//        a +=5;
-        String b = "";
-        for(int i = a.length()-1;i>=0;i--)
+        /**
+         * replace(char oldChar,char newChar)
+         */
+
+        String s9 = "Hello World";
+        String s10 = s9.replace('W','L');
+        System.out.println(s10);
+        s10 = s9.replace("World","dear");
+        System.out.println(s10);
+
+        /**
+         * split(String regEx)
+         * Split to Array
+         */
+        String text = "Hello/World/My/Dear";
+        String[] textSplit = text.split("/");
+        for(int i = 0; i < textSplit.length; i++)
         {
-            b += a.charAt(i);
-            //b.concat(String.valueOf(a.charAt(i)));
-        }
-//        System.out.print(b);
-//
-//        if(a.equals(b))
-//        {
-//            System.out.println("Strings are Palindrome");
-//        }
-//        else{
-//            System.out.println("Not a Palindrome");
-//        }
-
-        // while
-        boolean isPalindrome = true;
-        //int isPal = 1;
-        char[] pal = a.toCharArray();
-        int i = 0;
-        int k = pal.length-1;
-        while(i<pal.length && k>=0)
-        {
-            if(pal[i] != pal[k])
-            {
-                isPalindrome = false;
-               // isPal = 0;
-                break;
-            }
-            i++;
-            k--;
-        }
-        if(isPalindrome)
-        {
-            System.out.println("Palindrome");
-        }
-        else{
-            System.out.println("Not a Palindrome");
+            System.out.println(textSplit[i]);
         }
 
+        /**
+         * indexOf()
+         */
+
+        String s11 = "Coimbatore";
+        int pos = s11.indexOf("o");
+        System.out.println("Position "+pos);
+
+        /**
+         * LowerCase & UpperCase
+         */
+        String s12 = "gowtham";
+        System.out.println(s12.toUpperCase());
+        s12 = "GOWTHAM";
+        System.out.println(s12.toLowerCase());
+
+        /**
+         * trim
+         * It removes first and Last spaces
+         */
+
+        String s13 = " GOWTHAM ";
+        System.out.println("Before Trim => "+s13.length());
+        String s14 = s13.trim();
+        System.out.println("Before Trim => "+s14.length());
 
 
     }
