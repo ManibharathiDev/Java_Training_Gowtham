542bcee005096cb4131c61e9c4035f496a9bf4b2
Manibharathi <manibharath159@gmail.com>
2024-09-25 20:16:33 +0530

    Custom Arraylist

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,59 +2,33 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
 
-        ArrayList<Integer> age = new ArrayList<>();
-        ArrayList<String> name = new ArrayList<>();
-
-        for(int i = 0; i<100;i++)
+        ArrayList<Students> studentsArrayList = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        for(int i = 0; i < 5; i++)
         {
-            if(i == 49 || i == 68)
-            {
-                age.add(190); // Add New Value
-            }
-            else age.add(i+1);
+            Students students = new Students();
+            System.out.println("Enter the Student Name "+(i+1));
+            students.setName(s.nextLine());
+            System.out.println("Enter the Roll Number");
+            students.setRollNumber(s.nextInt());
+            System.out.println("Enter the Age");
+            students.setAge(s.nextInt());
+            studentsArrayList.add(students);
         }
-       //age.add(90);
-        //System.out.println("Age is "+age.get(100)); // Get - Index
 
-        for(int i = 0; i<100;i++)
+        for(int i = 0; i< studentsArrayList.size(); i++)
         {
-            System.out.println("Age is "+age.get(i)); // Get - Index
+            Students students = studentsArrayList.get(i);
+            System.out.println("Details - "+(i+1));
+            System.out.println("Name "+students.getName());
+            System.out.println("Roll Number "+students.getRollNumber());
+            System.out.println("Age "+students.getAge());
         }
-
-        System.out.println("Size of array list "+age.size());
-        System.out.println(age.get(8)); // return a 8th position value
-
-        if(age.contains(1900))
-        {
-            System.out.println("Yes");
-        }
-        else{
-            System.out.println("No");
-        }
-
-        //age.clear(); // Remove all values
-       // System.out.println("Size of array list "+age.size());
-
-        System.out.println("Index of => "+age.indexOf(56));
-
-//        if(age.size() == 0)
-//        {
-//
-//        }
-        if(age.isEmpty())
-        {
-            System.out.println("Empty");
-        }
-        else{
-            System.out.println("Not empty");
-        }
-
-        System.out.println(age.lastIndexOf(190));
-        age.remove(3); // remove
 
     }
 }
