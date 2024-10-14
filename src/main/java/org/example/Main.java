abe53edee877cca1afacfa680ab7872d2832afd2
Manibharathi <manibharath159@gmail.com>
2024-10-14 20:03:25 +0530

    Custom Arraylist

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
 
-        ArrayList<Students> studentsArrayList = new ArrayList<>();
+        /*ArrayList<Students> studentsArrayList = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         for(int i = 0; i < 5; i++)
         {
@@ -28,7 +28,45 @@ public class Main{
             System.out.println("Name "+students.getName());
             System.out.println("Roll Number "+students.getRollNumber());
             System.out.println("Age "+students.getAge());
+        }*/
+
+        /*ArrayList<String> studentArrayList = new ArrayList<>();
+        studentArrayList.add("Gowtham");
+        if(studentArrayList.contains("Gowtham"))
+        {
+            System.out.println("Dublicate Entry");
         }
+        else{
+            System.out.println("Added");
+        }*/
+
+        ArrayList<Students> studentsArrayList = new ArrayList<>();
+//        if(studentsArrayList.contains("gowtham"))
+        Students students = new Students();
+        students.setName("Gowtham");
+        studentsArrayList.add(students);
+
+        String name = "Gowthams";
+        Students students1 = null;
+        for(int i = 0; i<studentsArrayList.size();i++)
+        {
+            if(studentsArrayList.get(i).getName().equals(name))
+            {
+                students1 = studentsArrayList.get(i);
+                break;
+            }
+        }
+
+        if(students1 != null)
+        {
+            System.out.println("Data is Available");
+        }
+        else{
+            System.out.println("Data is not available");
+        }
+
+
+
 
     }
 }
